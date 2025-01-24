958d6f7d8780479f75c6677b3420272e95e88320
Vizziona <vizzionainc@gmail.com>
2025-01-25 00:06:08 +0200

    initial commit

--- platform/themes/hously/webpack.mix.js
+++ platform/themes/hously/webpack.mix.js
@@ -7,23 +7,24 @@ const source = 'platform/themes/' + directory
 const dist = 'public/themes/' + directory
 
 mix
-    .sass(source + '/assets/sass/icons.scss', dist + '/css')
-    .sass(source + '/assets/sass/style.scss', dist + '/css', {}, [
+    .sass(source + '/assets/sass/icons.scss', dist + '/css/icons.css')
+    .sass(source + '/assets/sass/style.scss', dist + '/css/style.css', {}, [
         tailwindcss(source + '/tailwind.config.js'),
     ])
-    .js(source + '/assets/js/app.js', dist + '/js')
-    .js(source + '/assets/js/wishlist.js', dist + '/js')
-    .js(source + '/assets/js/filter.js', dist + '/js')
-    .js(source + '/assets/js/review.js', dist + '/js')
-    .js(source + '/assets/js/script.js', dist + '/js')
+    .js(source + '/assets/js/app.js', dist + '/js/app.js')
+    .js(source + '/assets/js/wishlist.js', dist + '/js/wishlist.js')
+    .js(source + '/assets/js/filter.js', dist + '/js/filter.js')
+    .js(source + '/assets/js/review.js', dist + '/js/review.js')
+    .js(source + '/assets/js/script.js', dist + '/js/script.js')
 
 if (mix.inProduction()) {
     mix
-        .copy(dist + '/css/icons.css', source + '/public/css')
-        .copy(dist + '/css/style.css', source + '/public/css')
-        .copy(dist + '/js/app.js', source + '/public/js')
-        .copy(dist + '/js/wishlist.js', source + '/public/js')
-        .copy(dist + '/js/filter.js', source + '/public/js')
-        .copy(dist + '/js/review.js', source + '/public/js')
-        .copy(dist + '/js/script.js', source + '/public/js')
+        // Remove or update this line if necessary
+        // .copy(dist + '/css/icons.css', source + '/public/css/icons.css')
+        .copy(dist + '/css/style.css', source + '/public/css/style.css')
+        .copy(dist + '/js/app.js', source + '/public/js/app.js')
+        .copy(dist + '/js/wishlist.js', source + '/public/js/wishlist.js')
+        .copy(dist + '/js/filter.js', source + '/public/js/filter.js')
+        .copy(dist + '/js/review.js', source + '/public/js/review.js')
+        .copy(dist + '/js/script.js', source + '/public/js/script.js')
 }
